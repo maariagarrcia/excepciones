6f94268c36509c828da4fd8a22d4c0f1d0987dbe
maariagarrcia <mrrmn@myuax.com>
2022-04-03 15:50:50 +0200

    estructura

---
+++ input_email.py
@@ -0,0 +1,10 @@
+# Reglas de la sintaxis de una eMail ................ https://help.returnpath.com/hc/en-us/articles/220560587-What-are-the-rules-for-email-address-syntax-
+# Como validar un email con expresiones regulares ... https://parzibyte.me/blog/2018/12/04/comprobar-correo-electronico-python/
+
+### I M P O R T S
+import re
+
+# Valida si un cadena contiene un eMail válido
+# 
+def eMailTest(cadena):
+def eMailInput(descripcion):
---
+++ main.py
@@ -0,0 +1,14 @@
+### I M P O R T S
+
+
+
+#
+#   F U N C I O N E S
+#
+def test_email():
+
+
+
+#
+#   I N I C I O    P R O G R A M A
+#
